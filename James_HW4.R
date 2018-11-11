977c8cab1ca066f8baf897e7c1438e6551b860f4
Hongye Wu <hongyew12@gmail.com>
2018-11-11 17:53:36 -0500

    add graph observation write-ups

--- James_HW4.R
+++ James_HW4.R
@@ -15,32 +15,38 @@ Most_Common_Types
 ## crime types) by hour. Write a few sentences about the pattern you observe.
 theme_set(theme_bw())
 
-p <- ggplot(data = all_crimes)
-p + 
+p <- ggplot(data = all_crimes)+
   geom_bar(aes(x=factor(Hour))) +
   xlab("Hour of Day") +
   ylab("Crime Count (Aggregated across neighborhoods and crime types)")
-
+p
 
 ## Restrict to the five most common crime types, and plot the total number of crimes
 ## (aggregated across neighborhoods) for each crime type by hour (i.e., your plot should
-## have five lines). Write a few sentences about the pattern you observe.
+## have five lines). 
 
-# Stacked Bar Plot (I think looks better)
+# Shooting seems to be the most common crime out of the five most frequesnt crime types throughout the day, 
+# followed by gunfire and stabbing. It is not clear what the difference between gunfire and shooting is.
+# Crime counts drastically increases after 8pm. Most crimes occur between 8pm-11pm. After midnight, criminal instances
+# steadily decrease till around 7am, which is the hour with the lowest crime counts in a day. Then after 7am, the most common
+# types of crimes pick up again.
+
+# Stacked Bar Plot
 q <- ggplot(data = all_crimes %>%
               filter(Crime_Type %in% Most_Common_Types$Crime_Type))
-q + 
-  geom_bar(aes(x=factor(Hour), fill = Crime_Type)) +
+
+q+  geom_bar(aes(x=factor(Hour), fill = Crime_Type)) +
   xlab("Hour of Day") +
   ylab("Crime Count (Aggregated across neighborhoods)")
 
+
 # Line Plot
-q <- ggplot(data = all_crimes %>%
+l <- ggplot(data = all_crimes %>%
               filter(Crime_Type %in% Most_Common_Types$Crime_Type) %>%
               group_by(Hour,Crime_Type) %>%
               summarise(Crime_Count = n()))
 
-q +
+l +
   geom_line(aes(x = Hour, y = Crime_Count, color = Crime_Type)) +
   xlab("Hour of Day") +
   ylab("Crime Count (Aggregated across neighborhoods)")
@@ -49,9 +55,14 @@ q +
 ## Restrict to just the neighborhoods of Dorchester and Downtown, and plot the total
 ## number of crimes (aggregated across crime types (include all crime types, not just the
 ## top five)) for each of the two neighborhoods by hour (i.e., your plot should have two
-## lines). Write a few sentences about the pattern you observe.
+## lines). 
 
-# Stacked Bar Plot (I think looks better)
+# Dorchester has higher instances of crimes than the downtown area. For dorchester, most crimes occur between 9-11pm with 
+# a surge of crimes at 3pm. For downtown, most crimes occur between 3pm-10pm. It makes sense because downtown Boston is 
+# the financial district with mostly office buildings, while Dorchester is a predominantly residential neighborhood for 
+# people of color and a reputation for gang violence. 
+
+# Stacked Bar Plot
 g <- ggplot(data = all_crimes %>%
               filter(Neighborhood %in% c("dorchester","downtown")))
 
